410a692092244650ef3c01f87bad67f71696f4e3
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2021-11-12 14:32:21 +0530

    packages

---
+++ SavingsAccountPackage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.workshop;
+
+class SavingsAccount1 {
+    public SavingsAccount1( ) {
+    }
+
+    public void modifyInterestRate(double v) {
+    }
+
+    public void calculateMonthlyInterest() {
+    }
+}
